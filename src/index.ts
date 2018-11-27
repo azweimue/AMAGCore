8076a8363ee1138a2708ccf6ac560159e61d4ce5
Andreas Zweimüller <andreas.zweimueller@outlook.com>
2018-11-27 16:00:40 +0100

    1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import { getRandomString } from '@pnp/common';
-import { Web } from "@pnp/sp";
+import { Web } from '@pnp/sp';
 
 export const Greeter = (name: string) => `Hello ${getRandomString(20)}`;
 
-export function GetWebName(url:string) : string {
-    const web = new Web(url);
-    let name : string = "";
-    web.get().then((w) => {name = w.name;});
-    return name;
+export function GetWebName(url: string): string {
+  const web = new Web(url);
+  let name: string = '';
+  web.get().then(w => {
+    name = w.name;
+  });
+  return name;
 }
-
-
-
