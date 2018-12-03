5a8dc50b59456c29f8ec900011e202db54f659a6
Andreas Zweimüller <andreas.zweimueller@outlook.com>
2018-12-03 15:28:43 +0100

    1.0.9

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { getRandomString } from '@pnp/common';
 import { Web } from '@pnp/sp';
 import * as PropertyConfiguration from './PropertyConfiguration';
 
-
 export * from './PropertyConfiguration';
 
 export const Greeter = (name: string) => `Hello ${getRandomString(20)}`;
