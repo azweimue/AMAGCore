f25d33f500eb867dbc829a5d0e769a679a02f4ab
Andreas Zweimüller <andreas.zweimueller@outlook.com>
2018-12-03 11:34:04 +0100

    1.0.6

---
+++ src/PropertyConfiguration.ts
@@ -0,0 +1,23 @@
+export class PropertyConfiguration {
+  public GetPropertyValue(propertyName: string) {
+    this.GetPropertyValueAjax(propertyName)
+      .done(result => {
+        return result;
+      })
+      .fail(() => {
+        // An error occurred
+      });
+  }
+  private GetPropertyValueAjax(propertyName: string) {
+    return $.ajax({
+      headers: { accept: 'application/json;odata=verbose' },
+      type: 'GET',
+      url:
+        'https://intranettest.amag.at/_layouts/15/AMAG.Intranet.Core/CoreWebService.asmx/GetPropertyValue?$propertyName=Test',
+
+      // ,
+      // success: onCompleted,
+      // error: onError
+    });
+  }
+}
