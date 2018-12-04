e8da6892d0b3549eab000af8837e6ef298aaffba
Andreas Zweimüller <andreas.zweimueller@outlook.com>
2018-12-04 15:50:31 +0100

    1.0.18

--- src/PropertyConfiguration.ts
+++ src/PropertyConfiguration.ts
@@ -2,8 +2,8 @@ import { Promise } from 'es6-promise';
 import * as jquery from 'jquery';
 
 export class PropertyConfiguration {
-  public GetPropertyValue(propertyName: string, webURL: string) : Promise<string> {
-    const promiseObj : Promise<string>= new Promise((resolve: any, reject: any) => {
+  public GetPropertyValue(propertyName: string, webURL: string): Promise<string> {
+    const promiseObj: Promise<string> = new Promise((resolve: any, reject: any) => {
       const url = webURL + '/_vti_bin/CoreWebService.svc/GetPropertyValue/' + propertyName;
       const xhr = new XMLHttpRequest();
       xhr.open('GET', url, true);
