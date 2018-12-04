00c0c06c00d7de3fdf81b17432fc366754d6893d
Andreas Zweimüller <andreas.zweimueller@outlook.com>
2018-12-04 14:07:33 +0100

    GetPropertyValue

--- src/PropertyConfiguration.ts
+++ src/PropertyConfiguration.ts
@@ -1,30 +1,26 @@
 import * as jquery from 'jquery';
-import {Promise} from 'es6-promise';
+import { Promise } from 'es6-promise';
 
 export class PropertyConfiguration {
   public GetPropertyValue(propertyName: string, webURL: string) {
-    let promiseObj = new Promise((resolve : any, reject : any) => {
-      var url = webURL + "/_vti_bin/CoreWebService.svc/GetPropertyValue/" + propertyName; 
+    let promiseObj = new Promise((resolve: any, reject: any) => {
+      var url = webURL + "/_vti_bin/CoreWebService.svc/GetPropertyValue/" + propertyName;
       let xhr = new XMLHttpRequest();
-       xhr.open("GET", url, true,);
-       xhr.send();
-       xhr.onreadystatechange = function(){
-       if (xhr.readyState === 4){
-          if (xhr.status === 200){
-             console.log("xhr done successfully");
-             var resp = xhr.responseText;
-             var respJson = JSON.parse(resp);
-             resolve(respJson);
+      xhr.open("GET", url, true);
+      xhr.send();
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            var resp = xhr.responseText;
+            var respJson = JSON.parse(resp);
+            resolve(respJson);
           } else {
-             reject(xhr.status);
-             console.log("xhr failed");
+            reject(xhr.status);
           }
-       } else {
-          console.log("xhr processing going on");
-       }
-    }
-    console.log("request sent succesfully");
-   });
-  return promiseObj;
+        } else {
+        }
+      }
+    });
+    return promiseObj;
   }
 }
