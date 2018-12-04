58dede9d645bb637c4d4bde6596025302dabbc55
Andreas Zweimüller <andreas.zweimueller@outlook.com>
2018-12-04 14:10:36 +0100

    GetPropertyConfiguration

--- src/PropertyConfiguration.ts
+++ src/PropertyConfiguration.ts
@@ -1,23 +1,23 @@
-import * as jquery from 'jquery';
 import { Promise } from 'es6-promise';
+import * as jquery from 'jquery';
+
 
 export class PropertyConfiguration {
   public GetPropertyValue(propertyName: string, webURL: string) {
-    let promiseObj = new Promise((resolve: any, reject: any) => {
-      var url = webURL + "/_vti_bin/CoreWebService.svc/GetPropertyValue/" + propertyName;
-      let xhr = new XMLHttpRequest();
+    const promiseObj = new Promise((resolve: any, reject: any) => {
+      const url = webURL + "/_vti_bin/CoreWebService.svc/GetPropertyValue/" + propertyName;
+      const xhr = new XMLHttpRequest();
       xhr.open("GET", url, true);
       xhr.send();
-      xhr.onreadystatechange = function () {
+      xhr.onreadystatechange = () => {
         if (xhr.readyState === 4) {
           if (xhr.status === 200) {
-            var resp = xhr.responseText;
-            var respJson = JSON.parse(resp);
+            const resp = xhr.responseText;
+            const respJson = JSON.parse(resp);
             resolve(respJson);
           } else {
             reject(xhr.status);
           }
-        } else {
         }
       }
     });
