7cf43c79c5146188093bf7ab5276a16b97615110
Andreas Zweimüller <andreas.zweimueller@outlook.com>
2018-12-10 10:06:27 +0100

    Test

--- src/PropertyConfiguration.ts
+++ src/PropertyConfiguration.ts
@@ -1,8 +1,8 @@
 import { Promise } from 'es6-promise';
 import * as jquery from 'jquery';
 
-export class PropertyConfiguration {
-  public GetPropertyValue(propertyName: string, webURL: string): Promise<string> {
+export abstract class PropertyConfiguration {
+  public static GetPropertyValue(propertyName: string, webURL: string): Promise<string> {
     const promiseObj: Promise<string> = new Promise((resolve: any, reject: any) => {
       const url = webURL + '/_vti_bin/CoreWebService.svc/GetPropertyValue/' + propertyName;
       const xhr = new XMLHttpRequest();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
-import { getRandomString } from '@pnp/common';
-import { Web } from '@pnp/sp';
-import * as PropertyConfiguration from './PropertyConfiguration';
+//import {PropertyConfiguration} from './PropertyConfiguration';
 
-export * from './PropertyConfiguration';
-
-export const Greeter = (name: string) => `Hello ${getRandomString(20)}`;
-
-export async function GetWebName(url: string) {
-  const web = new Web(url);
-  const w = await web.get();
-  return w.Title;
-}
+export {PropertyConfiguration} from './PropertyConfiguration';
